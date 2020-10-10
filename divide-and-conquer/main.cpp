1964a07cffc34b8e852ebe81f20add5ceb8c44b4
Kremenevskiy <vkremenevskiy@gmail.com>
2020-10-10 19:00:11 +0300

    Visual main.cpp fix

--- divide-and-conquer/main.cpp
+++ divide-and-conquer/main.cpp
@@ -3,8 +3,13 @@
 
 int main() {
 //    std::vector<int> a {34, 4, -1, 10, -23};
-    std::vector<int> a {1, 2, -1};
-    std::cout << std::get<2>(findMaximumSubArray(a, 0, a.size()));
+    std::vector<int> a {1, -23, 323, 12, -21, -12, -122, 2134, -124};
+    std::tuple<int, int, int> findMaxSubArrSum = findMaximumSubArray(a, 0, a.size());
+    int leftBorder = std::get<0>(findMaxSubArrSum);
+    int rightBorder = std::get<1>(findMaxSubArrSum);
+    int maxSum = std::get<2>(findMaxSubArrSum);
+
+    std::cout << "Subarray is: arr[" << leftBorder << "] - arr[" << rightBorder << "] - with sum: " << maxSum << '\n';
 
     return 0;
 }
